dc0cdc04f46c4e745b9432bc71ed6d786888d001
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-03-31 00:05:38 +0530

    updated

--- fullstack-application/fullstack-application/src/main/java/com/example/fullstackapplication/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.fullstackapplication.exception;
-
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(Long id){
-        super("Could not found the user with id "+ id);
-    }
-}
